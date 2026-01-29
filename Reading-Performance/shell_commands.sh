8494443ce554e957eb0fe21435ce81de3a2a73d4
Wonder Agudah <85680026+Wonder717@users.noreply.github.com>
2026-01-29 14:06:20 +0000

    Fix formatting and add missing newline in shell commands script

--- Reading-Performance/shell_commands.sh
+++ Reading-Performance/shell_commands.sh
@@ -5,3 +5,6 @@ aws s3 mb s3://rp-data-project
 aws s3api put-object --bucket rp-data-project --key raw/
 aws s3api put-object --bucket rp-data-project --key processed/
 aws s3api put-object --bucket rp-data-project --key athena-results/
+
+#copy data from local to s3 bucket in raw folder
+aws s3 cp reading.parquet s3://rp-data-project/raw/ 
