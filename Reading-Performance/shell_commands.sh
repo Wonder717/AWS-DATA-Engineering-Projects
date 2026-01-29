c59b1a9b2132f314c8e2cdca7acf4ddd557bd540
Wonder Agudah <85680026+Wonder717@users.noreply.github.com>
2026-01-29 14:05:16 +0000

    Add commands to create S3 bucket and necessary folders for data project

--- Reading-Performance/shell_commands.sh
+++ Reading-Performance/shell_commands.sh
@@ -1,2 +1,7 @@
 #create s3 bucket
 aws s3 mb s3://rp-data-project
+
+#create the following folders in the s3 bucket
+aws s3api put-object --bucket rp-data-project --key raw/
+aws s3api put-object --bucket rp-data-project --key processed/
+aws s3api put-object --bucket rp-data-project --key athena-results/
